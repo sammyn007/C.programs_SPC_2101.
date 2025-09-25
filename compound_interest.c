e53c43c2a9ecc885a771e0238bc3aef99169cdb3
sammyn007 <sammyn08m@gmail.com>
2025-09-25 11:59:25 +0300

    Add files via upload

---
+++ compound_interest.c
@@ -0,0 +1,35 @@
+/*
+Name:Sammy Njuguna
+Reg.no.:CT101/G/28858/25
+Date:19 sep 2025
+Description:compound_interest
+a=p(1+r/100/n)^(n*t)
+*/
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    int n;
+    float principal,rate,time,amount,compound_interest;
+
+//prompt user to enter
+    printf("Enter principal amount:");
+    scanf("%f",&principal);
+
+    printf("\nEnter interest rate:");
+    scanf("%f",&rate);
+
+    printf("\nEnter total number of years:");
+    scanf("%f",&time);
+
+    printf("\nEnter number of compounding per year:");
+    scanf("%d",&n);
+
+    amount=principal*pow((1+rate/100/n),(n*time));
+    compound_interest=amount - principal;
+
+    printf("\nCompound_interest is: %2f",compound_interest);
+    return 0;
+}
+
+    
